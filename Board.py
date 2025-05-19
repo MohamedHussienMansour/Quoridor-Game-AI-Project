aae90b6cb0ed1710e271786c3ff273f531bb0ad2
Marwan osama <marwan.osama@scandiweb.com>
2025-05-19 17:28:58 +0300

    merge master and format files (autopip8)

---
+++ Board.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Board:
+    def __init__(self, no_players=2, dim=9):
+        self.dimPawnBoard = dim
+        self.dimWallBoard = self.dimPawnBoard - 1
+        self.dimBoard = self.dimPawnBoard + self.dimWallBoard
+        self.board = np.zeros((self.dimBoard, self.dimBoard))
+        self.no_players = no_players
+        self.codesPlayers = range(1, no_players + 1)
+
+
+if __name__ == "__main__":
+    b = Board()
+    print(b.board)
--- main.py
+++ main.py
@@ -24,16 +24,17 @@ PLAYER_2_CODE = 2
 HORIZONTAL_CONNECTOR_CODE = 1
 VERTICAL_CONNECTOR_CODE = 2
 
+
 class GameGUI:
     def __init__(
-            self,
-            board,
-            screen_width=800,
-            screen_height=600,
-            game_size=9,
-            margin=10,
-            wall_to_wall_gap=8
-        ):
+        self,
+        board,
+        screen_width=800,
+        screen_height=600,
+        game_size=9,
+        margin=10,
+        wall_to_wall_gap=8,
+    ):
         pygame.init()
         self.board = board
         self.screen_width = screen_width
@@ -49,7 +50,9 @@ class GameGUI:
         self.game_x_pos = (self.screen_width - self.game_surface_side) / 2
         self.game_y_pos = (self.screen_height - self.game_surface_side) / 2
 
-        self.cell_side = round((self.game_surface_side - ((game_size + 1) * self.margin)) / self.game_size)
+        self.cell_side = round(
+            (self.game_surface_side - ((game_size + 1) * self.margin)) / self.game_size
+        )
         self.player_radius = round(self.cell_side * 0.4)
 
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
@@ -61,12 +64,15 @@ class GameGUI:
         x_offset = mouse_pos[0] - self.game_x_pos
         y_offset = mouse_pos[1] - self.game_y_pos
 
-        if not (0 <= x_offset < self.game_surface_side and 0 <= y_offset < self.game_surface_side):
+        if not (
+            0 <= x_offset < self.game_surface_side
+            and 0 <= y_offset < self.game_surface_side
+        ):
             return None
 
         for y in range(len(self.board)):
             for x in range(len(self.board[y])):
-                is_wall = (y % 2 != x % 2)  # wall positions only
+                is_wall = y % 2 != x % 2  # wall positions only
                 if not is_wall:
                     continue
 
@@ -74,7 +80,9 @@ class GameGUI:
                 width = self.margin if x % 2 == 1 else self.cell_side
                 height = self.margin if y % 2 == 1 else self.cell_side
 
-                rect = pygame.Rect(left + self.game_x_pos, top + self.game_y_pos, width, height)
+                rect = pygame.Rect(
+                    left + self.game_x_pos, top + self.game_y_pos, width, height
+                )
                 if rect.collidepoint(mouse_pos):
                     return (x, y)
         return None
@@ -99,7 +107,9 @@ class GameGUI:
                 self.board[y][x] = 1
                 my = (fy + y) // 2
                 mx = (fx + x) // 2
-                self.board[my][mx] = HORIZONTAL_CONNECTOR_CODE if y == fy else VERTICAL_CONNECTOR_CODE
+                self.board[my][mx] = (
+                    HORIZONTAL_CONNECTOR_CODE if y == fy else VERTICAL_CONNECTOR_CODE
+                )
                 self.first_wall_part = None
 
     def is_valid_second_part(self, x, y, fx, fy):
@@ -114,9 +124,9 @@ class GameGUI:
 
             # Prevent crossing vertical wall (check above and below the midpoint)
             if (
-                self.board[my - 1][mx] == 1 and
-                self.board[my][mx] == 2 and
-                self.board[my + 1][mx] == 1
+                self.board[my - 1][mx] == 1
+                and self.board[my][mx] == 2
+                and self.board[my + 1][mx] == 1
             ):
                 return False
 
@@ -128,9 +138,9 @@ class GameGUI:
 
             # Prevent crossing horizontal wall (check left and right of midpoint)
             if (
-                self.board[my][mx - 1] == 1 and
-                self.board[my][mx] == 1 and
-                self.board[my][mx + 1] == 1
+                self.board[my][mx - 1] == 1
+                and self.board[my][mx] == 1
+                and self.board[my][mx + 1] == 1
             ):
                 return False
         else:
@@ -139,8 +149,16 @@ class GameGUI:
         return True
 
     def map_board_pos(self, x, y):
-        left = (x // 2) * (self.cell_side + self.margin) + (x % 2) * self.cell_side + self.margin
-        top = (y // 2) * (self.cell_side + self.margin) + (y % 2) * self.cell_side + self.margin
+        left = (
+            (x // 2) * (self.cell_side + self.margin)
+            + (x % 2) * self.cell_side
+            + self.margin
+        )
+        top = (
+            (y // 2) * (self.cell_side + self.margin)
+            + (y % 2) * self.cell_side
+            + self.margin
+        )
 
         return left, top
 
@@ -155,20 +173,32 @@ class GameGUI:
 
         left, top = self.map_board_pos(x, y)
 
-        left = left + self.wall_to_wall_gap/2 if is_horizontal_wall else left
-        top = top + self.wall_to_wall_gap/2 if is_vertical_wall else top
-        height = self.cell_side - self.wall_to_wall_gap if is_vertical_wall else self.margin
-        width = self.margin if is_vertical_wall else self.cell_side - self.wall_to_wall_gap
+        left = left + self.wall_to_wall_gap / 2 if is_horizontal_wall else left
+        top = top + self.wall_to_wall_gap / 2 if is_vertical_wall else top
+        height = (
+            self.cell_side - self.wall_to_wall_gap if is_vertical_wall else self.margin
+        )
+        width = (
+            self.margin if is_vertical_wall else self.cell_side - self.wall_to_wall_gap
+        )
 
         return left, top, width, height
 
     def get_wall_connector(self, x, y, is_horizontal_connector, is_vertical_connector):
         left, top = self.map_board_pos(x, y)
 
-        left = left - self.wall_to_wall_gap/2 if is_horizontal_connector else left
-        top = top - self.wall_to_wall_gap/2 if is_vertical_connector else top
-        width = self.margin if is_vertical_connector else self.margin + self.wall_to_wall_gap
-        height = self.margin if is_horizontal_connector else self.margin + self.wall_to_wall_gap
+        left = left - self.wall_to_wall_gap / 2 if is_horizontal_connector else left
+        top = top - self.wall_to_wall_gap / 2 if is_vertical_connector else top
+        width = (
+            self.margin
+            if is_vertical_connector
+            else self.margin + self.wall_to_wall_gap
+        )
+        height = (
+            self.margin
+            if is_horizontal_connector
+            else self.margin + self.wall_to_wall_gap
+        )
 
         return left, top, width, height
 
@@ -192,9 +222,9 @@ class GameGUI:
             elif x - 2 == fx:
                 connector_props = self.get_wall_connector(x - 1, y, True, False)
             elif y + 2 == fy:
-                connector_props = self.get_wall_connector(x , y + 1, False, True)
+                connector_props = self.get_wall_connector(x, y + 1, False, True)
             elif y - 2 == fy:
-                connector_props = self.get_wall_connector(x , y - 1, False, True)
+                connector_props = self.get_wall_connector(x, y - 1, False, True)
 
             if connector_props:
                 left, top, width, height = connector_props
@@ -221,7 +251,10 @@ class GameGUI:
             for x in range(len(self.board[y])):
                 is_cell = y % 2 == 0 and x % 2 == 0
                 is_horizontal_wall, is_vertical_wall = self.check_wall_type(x, y)
-                is_connector = not (is_cell or is_vertical_wall or is_horizontal_wall) and self.board[y][x] > 0
+                is_connector = (
+                    not (is_cell or is_vertical_wall or is_horizontal_wall)
+                    and self.board[y][x] > 0
+                )
 
                 left, top = self.map_board_pos(x, y)
                 width = self.cell_side
@@ -229,21 +262,43 @@ class GameGUI:
 
                 if is_cell:
                     cell = pygame.Rect(left, top, width, height)
-                    pygame.draw.rect(surface, pygame.Color("#611111"), cell, border_radius=round(self.cell_side*0.1))
+                    pygame.draw.rect(
+                        surface,
+                        pygame.Color("#611111"),
+                        cell,
+                        border_radius=round(self.cell_side * 0.1),
+                    )
 
                 if self.board[y][x] > 0 and is_cell:
-                    player_pos = (left + round(self.cell_side/2), top + round(self.cell_side/2))
-                    player_color = pygame.Color("#FFAAAA") if self.board[y][x] == PLAYER_1_CODE else pygame.Color("#FF0000")
+                    player_pos = (
+                        left + round(self.cell_side / 2),
+                        top + round(self.cell_side / 2),
+                    )
+                    player_color = (
+                        pygame.Color("#FFAAAA")
+                        if self.board[y][x] == PLAYER_1_CODE
+                        else pygame.Color("#FF0000")
+                    )
 
-                    pygame.draw.circle(surface, player_color, player_pos, self.player_radius)
+                    pygame.draw.circle(
+                        surface, player_color, player_pos, self.player_radius
+                    )
                 elif (is_vertical_wall or is_horizontal_wall) and self.board[y][x] > 0:
                     left, top, width, height = self.get_wall(x, y)
-                    pygame.draw.rect(surface, pygame.Color("#F3F3F3"), pygame.Rect(left, top, width, height))
+                    pygame.draw.rect(
+                        surface,
+                        pygame.Color("#F3F3F3"),
+                        pygame.Rect(left, top, width, height),
+                    )
                 elif is_connector:
                     is_vertical_connector = self.board[y][x] == VERTICAL_CONNECTOR_CODE
-                    is_horizontal_connector = self.board[y][x] == HORIZONTAL_CONNECTOR_CODE
+                    is_horizontal_connector = (
+                        self.board[y][x] == HORIZONTAL_CONNECTOR_CODE
+                    )
 
-                    left, top, width, height = self.get_wall_connector(x, y, is_horizontal_connector, is_vertical_connector)
+                    left, top, width, height = self.get_wall_connector(
+                        x, y, is_horizontal_connector, is_vertical_connector
+                    )
 
                     connector = pygame.Rect(left, top, width, height)
                     pygame.draw.rect(surface, pygame.Color("#F3F3F3"), connector)
